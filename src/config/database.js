66b81a795d96c88b1c291c84a852f93956fab2d5
Jonathan <jonathan@Jonathans-MacBook-Pro.local>
2020-08-22 21:47:37 -0300

    teste circle ci

--- src/config/database.js
+++ src/config/database.js
@@ -16,7 +16,7 @@ module.exports = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   dialect: process.env.DB_DIALECT || "mysql",
-  storage: "./__tests__/database.sqlite",
+  storage: "../__tests__/database.sqlite",
   operatorsAliases: false,
   logging: false,
   define: {
